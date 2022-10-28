f561dab34274ddeb5c77a472a5334b37498a1507
Nick Stanford <nick@reach.sh>
2022-10-28 14:42:55 -0600

    ETH busted

--- index.mjs
+++ index.mjs
@@ -1,10 +1,11 @@
 import { loadStdlib } from "@reach-sh/stdlib";
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib({REACH_NO_WARN: 'Y'});
+const MAX = 10;
 
 const accA = await stdlib.newTestAccount(stdlib.parseCurrency(5000));
 const ctcA = accA.contract(backend);
-const tickets = await stdlib.launchToken(accA, "Tickets", "TIK", {supply: 500});
+const tickets = await stdlib.launchToken(accA, "Tickets", "TIK", {supply: MAX});
 const ticketTok = tickets.id;
 console.log('Welcome to the ticket distributor\nLets get you a ticket');
 
@@ -16,7 +17,7 @@ const startBuyers = async () => {
     const succ = await ctc.apis.Buyer.buyTicket();
     console.log(`${who} purchased a ticket is ${succ}`);
   }// end of runBuyers
-  for(let i = 0; i < 500; i++){
+  for(let i = 0; i < MAX; i++){
     console.log(`Test ${i}`);
     await runBuyers('Buyer');
   }
@@ -26,7 +27,7 @@ await Promise.all([
   backend.Admin(ctcA, {
     cost: stdlib.parseCurrency(10),
     token: ticketTok,
-    supply: 500,
+    supply: MAX,
     ready: (contract) => {
       console.log(`Ready at contract: ${contract}`);
       startBuyers();
