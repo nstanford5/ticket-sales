86cb6b7ace4625f52865102803bef71e9a427749
Nick Stanford <nick@reach.sh>
2023-03-13 15:08:03 -0600

    updates

--- truffle6/contracts/myERC.sol
+++ truffle6/contracts/myERC.sol
@@ -5,29 +5,254 @@ import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721UR
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
-contract myERC is ERC721, ERC721URIStorage, Ownable {
+// imports from open zeppelin
+import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Address.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Context.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
+import "../node_modules/@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+contract myERC is Context, ERC165, IERC721, IERC721Metadata, Ownable {
   using Counters for Counters.Counter;
+  using Address for address;
+  using Strings for uint256;
 
   Counters.Counter private _tokenIdCounter;
+  string private _name;
+  string private _symbol;
 
-  constructor() ERC721("myERC", "MERC"){}
+  mapping(uint256 => address) private _owners;
+  mapping(address => uint256) private _balances;
+  mapping(uint256 => address) private _tokenApprovals;
+  mapping(address => mapping(address => bool)) private _operatorApprovals;
 
-  function safeMint(address to, string memory uri) public onlyOwner {
-    uint256 tokenId = _tokenIdCounter.current();
-    _tokenIdCounter.increment();
-    _safeMint(to, tokenId);
-    _setTokenURI(tokenId, uri);
+  constructor(){
+    _name = 'myERC';
+    _symbol = 'MERC';
   }
 
-  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage){
-    super._burn(tokenId);
+  function supportsInterface(bytes4 interfaceId) public view virtual override (ERC165, IERC165) returns (bool) {
+    return 
+      interfaceId == type(IERC721).interfaceId ||
+      interfaceId == type(IERC721Metadata).interfaceId ||
+      super.supportsInterface(interfaceId);
+  }
+
+  function balanceOf(address owner) public view virtual override (IERC721) returns (uint256) {
+    require(owner != address(0), "ERC721: address zero is not a valid owner");
+    return _balances[owner];
+  }
+
+  function ownerOf(uint256 tokenId) public view virtual override (IERC721) returns (address) {
+    address owner = _ownerOf(tokenId);
+    require(owner != address(0), "ERC721: invalid token ID");
+    return owner;
+  }
+
+  function name() public view virtual override (IERC721Metadata) returns (string memory) {
+    return _name;
+  }
+
+  function symbol() public view virtual override (IERC721Metadata) returns (string memory) {
+    return _symbol;
   }
 
   function tokenURI(uint256 tokenId)
     public
     view
-    override(ERC721, ERC721URIStorage)
+    override(IERC721Metadata)
     returns (string memory) {
-      return super.tokenURI(tokenId);
+      _requireMinted(tokenId);
+      string memory baseURI = _baseURI();
+      return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
     }
+
+  function _baseURI() internal view virtual returns (string memory) {
+    return "";
+  }
+
+  function approve(address to, uint256 tokenId) public virtual override (IERC721) {
+    address owner = _ownerOf(tokenId);
+    require (to != owner, "ERC721: approval to current owner");
+
+    require(
+      _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
+      "ERC721: approve caller is not token owner or approved for all"
+    );
+  }
+
+  function getApproved(uint256 tokenId) public view virtual override (IERC721) returns (address) {
+    _requireMinted(tokenId);
+    return _tokenApprovals[tokenId];
+  }
+
+  function setApprovalForAll(address operator, bool approved) public virtual override (IERC721) {
+    _setApprovalForAll(_msgSender(), operator, approved);
+  }
+
+  function isApprovedForAll(address owner, address operator) public view virtual override (IERC721) returns (bool) {
+    return _operatorApprovals[owner][operator];
+  }
+
+  function transferFrom(address from, address to, uint256 tokenId) public virtual override (IERC721){
+    require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
+
+    _transfer(from, to, tokenId);
+  }
+
+  function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override (IERC721) {
+    safeTransferFrom(from, to, tokenId, "");
+  }
+
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public virtual override (IERC721) {
+    require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
+    _safeTransfer(from, to, tokenId, data);
+  }
+
+  function _safeTransfer(address from, address to, uint256 tokenId, bytes memory data) internal virtual {
+    _transfer(from, to, tokenId);
+    require(_checkOnERC721Received(from, to, tokenId, data), "ERC721: transfer to non ERC721Receiver implementer");
+  }
+
+  function _ownerOf(uint256 tokenId) internal view virtual returns (address) {
+    return _owners[tokenId];
+  }
+
+  function _exists(uint256 tokenId) internal view virtual returns (bool) {
+    return _ownerOf(tokenId) != address(0);
+  }
+
+  function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
+    address owner = _ownerOf(tokenId);
+    return (spender == owner || isApprovedForAll(owner, spender) || getApproved(tokenId) == spender);
+  }
+
+  function _safeMint(address to, uint256 tokenId) internal virtual {
+    _safeMint(to, tokenId, "");
+  }
+
+  function _safeMint(address to, uint256 tokenId, bytes memory data) internal virtual {
+    _mint(to, tokenId);
+    require(
+      _checkOnERC721Received(address(0), to, tokenId, data),
+      "ERC721: transfer to non ERC721Receiver implementer"
+    );
+  }
+
+  function _mint(address to, uint256 tokenId) internal virtual {
+    require(to != address(0), "ERC721: mint to zero address");
+    require(!_exists(tokenId), "ERC721: token already minted");
+
+    _beforeTokenTransfer(address(0), to, tokenId, 1);
+
+    require(!_exists(tokenId), "ERC721: token already minted");
+
+    unchecked {
+      _balances[to] += 1;
+    }
+
+    _owners[tokenId] = to;
+
+    emit Transfer(address(0), to, tokenId);
+
+    _afterTokenTransfer(address(0), to, tokenId, 1);
+  }
+
+  function _burn(uint256 tokenId) internal virtual {
+    address owner = _ownerOf(tokenId);
+    _beforeTokenTransfer(owner, address(0), tokenId, 1);
+
+    owner = _ownerOf(tokenId);
+
+    delete _tokenApprovals[tokenId];
+
+    unchecked {
+      _balances[owner] -= 1;
+    }
+    
+    delete _owners[tokenId];
+
+    emit Transfer(owner, address(0), tokenId);
+
+    _afterTokenTransfer(owner, address(0), tokenId, 1);
+  }
+
+  function _transfer(address from, address to, uint256 tokenId) internal virtual {
+    require(_ownerOf(tokenId) == from , "ERC721: transfer from incorrect owner");
+    require(to != address(0), "ERC721: transfer to the zero address");
+
+    _beforeTokenTransfer(from, to, tokenId, 1);
+
+    require(_ownerOf(tokenId) == from, "ERC721: transfer from incorrect owner");
+
+    delete _tokenApprovals[tokenId];
+
+    unchecked {
+      _balances[from] -= 1;
+      _balances[to] += 1;
+    }
+
+    _owners[tokenId] = to;
+
+    emit Transfer(from, to, tokenId);
+
+    _afterTokenTransfer(from, to, tokenId, 1);
+  }
+
+  function _approved(address to, uint256 tokenId) internal virtual {
+    _tokenApprovals[tokenId] = to;
+    emit Approval(_ownerOf(tokenId), to, tokenId);
+  }
+
+  function _setApprovalForAll(address owner, address operator, bool approved) internal virtual{
+    require(owner != operator, "ERC721: approve to caller");
+    _operatorApprovals[owner][operator] = approved;
+    emit ApprovalForAll(owner, operator, approved);
+  }
+
+  function _requireMinted(uint256 tokenId) internal view virtual {
+    require(_exists(tokenId), "ERC721: invalid token ID");
+  }
+
+  function _checkOnERC721Received(
+    address from, address to, uint256 tokenId, bytes memory data)
+    private returns (bool) {
+      if(to.isContract()) {
+        try IERC721Receiver(to).onERC721Received(_msgSender(), from, tokenId, data) returns (bytes4 retval) {
+          return retval == IERC721Receiver.onERC721Received.selector;
+        } catch (bytes memory reason) {
+          if (reason.length == 0) {
+            revert("ERC721: transfer to non ERC721Receiver implementer");
+          } else {
+            assembly {
+              revert(add(32, reason), mload(reason))
+            }
+          }
+        }
+      } else {
+        return true;
+      }
+    }
+
+  function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal virtual {}
+  function _afterTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal virtual {}
+
+  function __unsafe_increaseBalance(address account, uint256 amount) internal {
+    _balances[account] += amount;
+  }
+
+  function safeMint(address to) public onlyOwner {
+    uint256 tokenId = _tokenIdCounter.current();
+    _tokenIdCounter.increment();
+    _safeMint(to, tokenId);
+    //_setTokenURI(tokenId, uri);
+  }
+
+  // function _burn(uint256 tokenId) internal override(ERC721URIStorage){
+  //   super._burn(tokenId);
+  // }
+
+  
 }
--- truffle6/test/myERC.js
+++ truffle6/test/myERC.js
@@ -23,26 +23,27 @@ contract('myERC', () => {
       console.log(`The test failed successfully: ${e}`)
     }
   })
-  /**
+    /**
    * Test 3
    */
   it('should mint, return balance, check tokenId for owner', async () => {
     const storage = await myERC.new();
     // cannot mint to the zero address
     try {
-      await storage.safeMint('0x0000000000000000000000000000000000000000', 0);
+      await storage.safeMint('0x0000000000000000000000000000000000000000');
     } catch (e) {
       console.log(`Mint test failed successfully: ${e}`);
     }
 
-    await storage.safeMint('0x1028c139157ab9be0eb649c6fc10fb792b21cb67', 0);
+    // safeMint is not a function
+    await storage.safeMint('0x1028c139157ab9be0eb649c6fc10fb792b21cb67');
     const amt = await storage.balanceOf('0x1028c139157ab9be0eb649c6fc10fb792b21cb67');
     assert(amt.toString() === '1', "balance wrong");
 
     const addr = await storage.ownerOf(0);
     assert(Number(addr) == '0x1028c139157ab9be0eb649c6fc10fb792b21cb67', "address wrong");
   })
-  /**
+    /**
    * Test 4
    */
   it('should mint a collection', async () => {
@@ -61,7 +62,7 @@ contract('myERC', () => {
     ]
 
     for(let i = 0; i < 10; i++){
-      await storage.safeMint(addresses[i], i);
+      await storage.safeMint(addresses[i]);
       const amt = await storage.balanceOf(addresses[i]);
       assert(amt.toString() === '1', "balance wrong");
 
@@ -69,13 +70,13 @@ contract('myERC', () => {
       assert(Number(addr) == addresses[i], "address wrong");
     }
   })
-  /**
-   * Test 5
-   */
+  //   /**
+//    * Test 5
+//    */
   it('should transfer NFT', async () => {
     const storage = await myERC.new();
 
-    await storage.safeMint('0x75b8304824832f0d997345bdc717103d9a2fb99b', 0);
+    await storage.safeMint('0x75b8304824832f0d997345bdc717103d9a2fb99b');
     await storage.transferFrom('0x75b8304824832f0d997345bdc717103d9a2fb99b', '0xc28569362a51520be4817db263d180ecf4973c24', 0);
     const owner = await storage.ownerOf(0);
     assert(Number(owner) == '0xc28569362a51520be4817db263d180ecf4973c24', "transfer failed");
@@ -88,7 +89,7 @@ contract('myERC', () => {
       console.log(`The transfer test failed successfuly: ${e}`);
     }
   })
-  /**
+    /**
    * Test 6
    */
   it('should test interface', async () => {
@@ -104,52 +105,65 @@ contract('myERC', () => {
     const b3 = await storage.supportsInterface('0x12345678');
     console.log(`Support for this random number is ${b3}`);
   })
+  /**
+   * Test 7
+   */
   it('should mint a collection to a single owner', async () => {
-    const storage = await myERC.new();
+  const storage = await myERC.new();
 
-    for(let i = 0; i < 50; i++){
-      await storage.safeMint('0x75b8304824832f0d997345bdc717103d9a2fb99b', i);
-    }
+  for(let i = 0; i < 50; i++){
+    await storage.safeMint('0x75b8304824832f0d997345bdc717103d9a2fb99b');
+  }
 
-    const balance = await storage.balanceOf('0x75b8304824832f0d997345bdc717103d9a2fb99b');
-    assert(balance.toString() === '50');
+  const balance = await storage.balanceOf('0x75b8304824832f0d997345bdc717103d9a2fb99b');
+  assert(balance.toString() === '50');
   })
+  /**
+   * Test 9 -- this test needs more work
+   */
   it('should approve non-owners', async () => {
     const storage = await myERC.new();
 
-    await storage.safeMint('0x75b8304824832f0d997345bdc717103d9a2fb99b', 0);
+    await storage.safeMint('0x75b8304824832f0d997345bdc717103d9a2fb99b');
+    const own = await storage.ownerOf(0);
+    console.log(`This is the owner: ${own}`);
     // here is where we input the Reach contract address to spend the token
-    await storage.setApprovalForAll('0xc28569362a51520be4817db263d180ecf4973c24', true);
+    await storage.approve('0xc28569362a51520be4817db263d180ecf4973c24', 0);
 
-    const b = await storage.isApprovedForAll('0x75b8304824832f0d997345bdc717103d9a2fb99b', '0xc28569362a51520be4817db263d180ecf4973c24');
-    console.log(`Approval is: ${b}`);
+    const addr = await storage.getApproved(0);
+    console.log(`This address is approved: ${addr}`);
   })
-})
+})// end of tests
+  
 
-/**
- * 
-(0) 0x75b8304824832f0d997345bdc717103d9a2fb99b
-(1) 0xc28569362a51520be4817db263d180ecf4973c24
-(2) 0xb9e7d3e4eb204b51ed14cdf1460e01e43f0a995b
-(3) 0x6f76e1e1c094b4fbfb9edf1165e96adee76aef9d
-(4) 0x0f52c06aa9c86fd61a3af9536bb23bd29c31a8b2
-(5) 0xba090a5a70db5ccd915000429e1260cd9d52f9f0
-(6) 0x5dfdb8b87d543a83fe213c4a42f6042129330063
-(7) 0x48ffe001c7644ccb32cee2b506a52d5869acfad9
-(8) 0xf7f6af2236bb0e384ba07b5e6d682ce2e18b09ee
-(9) 0xdaaeb2ef11c92575eca3fc051c15657b1e70fbca
- */
 
-/**
- * 
-(0) 266dc6b529e8519d8ccbd9e7befb1ed23337279c477e847f04e1c79ed72db861
-(1) 99449834cf8db3e3402c7fc65070dd8b0143c4bc9e06304a6d35a22113372a10
-(2) ff95a1d6fdf7052beaa36b5cffea3cafcb1d96079db61feb4e77fe9944c4e33c
-(3) 83dd295e01df36a2e6677c324325460ec7ed1db7368c7a0f6189c8e3e1836295
-(4) 624796558b1522af7179bed759382cea17c610d55043e876e5c013cd267e5d2a
-(5) 475f70836d5a733e2384a2d8e69a07dc056d0267c8c84b1fe0a0e7adfacc07e9
-(6) c6b5309491970c1a876044c8a097e9ba8adc234adfa705b5c75a7688e021bfc2
-(7) dbd2fbb2f02c022f360d5d98d228dd98f3d1de549e18de461a9bb7ea9547b317
-(8) a24a703cf06a85b54fca9dbc2bee5a203167480c384bbf9f40560cfd12cbfbe2
-(9) 9c903aa8b98a0b4484d1875190b00f01f2749fed94228464e54fad3a29335be3
- */
+
+
+
+  
+
+
+
+// (0) 0x75b8304824832f0d997345bdc717103d9a2fb99b
+// (1) 0xc28569362a51520be4817db263d180ecf4973c24
+// (2) 0xb9e7d3e4eb204b51ed14cdf1460e01e43f0a995b
+// (3) 0x6f76e1e1c094b4fbfb9edf1165e96adee76aef9d
+// (4) 0x0f52c06aa9c86fd61a3af9536bb23bd29c31a8b2
+// (5) 0xba090a5a70db5ccd915000429e1260cd9d52f9f0
+// (6) 0x5dfdb8b87d543a83fe213c4a42f6042129330063
+// (7) 0x48ffe001c7644ccb32cee2b506a52d5869acfad9
+// (8) 0xf7f6af2236bb0e384ba07b5e6d682ce2e18b09ee
+// (9) 0xdaaeb2ef11c92575eca3fc051c15657b1e70fbca
+
+
+
+// (0) 266dc6b529e8519d8ccbd9e7befb1ed23337279c477e847f04e1c79ed72db861
+// (1) 99449834cf8db3e3402c7fc65070dd8b0143c4bc9e06304a6d35a22113372a10
+// (2) ff95a1d6fdf7052beaa36b5cffea3cafcb1d96079db61feb4e77fe9944c4e33c
+// (3) 83dd295e01df36a2e6677c324325460ec7ed1db7368c7a0f6189c8e3e1836295
+// (4) 624796558b1522af7179bed759382cea17c610d55043e876e5c013cd267e5d2a
+// (5) 475f70836d5a733e2384a2d8e69a07dc056d0267c8c84b1fe0a0e7adfacc07e9
+// (6) c6b5309491970c1a876044c8a097e9ba8adc234adfa705b5c75a7688e021bfc2
+// (7) dbd2fbb2f02c022f360d5d98d228dd98f3d1de549e18de461a9bb7ea9547b317
+// (8) a24a703cf06a85b54fca9dbc2bee5a203167480c384bbf9f40560cfd12cbfbe2
+// (9) 9c903aa8b98a0b4484d1875190b00f01f2749fed94228464e54fad3a29335be3
